9e9c82fb21201810ea4cffe7430071caadcbd8b9
Refreshing Squares <refreshing2@RS-Desk-2.(none)>
2013-06-26 11:20:09 +0530

    adding Django installer

--- install.sh
+++ install.sh
@@ -4,7 +4,16 @@ sudo apt-get install php5 php5-mysql mysql-server-5.5  apache2
 cd /var/
 sudo chmod 777 www/
 cd www
+wget http://www.sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/4.0.3/phpMyAdmin-4.0.3-all-languages.tar.bz2
 tar -jxf phpMyAdmin-4.0.3-all-languages.tar.bz2 -C ./
 rm -f phpMyAdmin-4.0.3-all-languages.tar.bz2
 mv phpMyAdmin-4.0.3-all-languages pma
 ## This needs to be added to php.ini - "extension=mysqli.so"
+
+#Django Install
+sudo app-get install pip
+pip install Django
+
+#Python packages
+sudo apt-get install python-mysqldb
+
