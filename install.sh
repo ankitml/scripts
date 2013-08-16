7cbf7bc27653bc251f9ac85f9c50990a7fa4657e
singlas <shashank.singla@gmail.com>
2013-08-16 21:22:24 +0530

    new packages

--- install.sh
+++ install.sh
@@ -19,7 +19,8 @@ sudo pip install Django
 
 #Django deployment
 sudo apt-get install libapache2-mod-wsgi
-sudo pip install django-tinymce django-localflavor PIL south
 
 #Python packages
-sudo apt-get install python-mysqldb python-tk python-beautifulsoup python-requests 
+sudo apt-get install python-mysqldb python-tk python-beautifulsoup python-requests libjpeg-dev 
+sudo pip install django-tinymce django-localflavor PIL south requests_oauthlib
+pip install flask Scrappy httplib2
