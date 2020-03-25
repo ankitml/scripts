c98d2f3e34421d11594e4f69a918539524222d3d
Ankit <ankitml@gmail.com>
2020-03-25 12:59:19 -0400

    python3.7 compatible short syntax async run

--- python/async/first.py
+++ python/async/first.py
@@ -4,13 +4,12 @@ import aiohttp
 URL_TEMPLATE = "https://hacker-news.firebaseio.com/v0/item/{}.json"
 
 
-async def main(loop, post_id):
+async def main(post_id):
     await asyncio.sleep(1)
     return 5
 
 
 if __name__ == '__main__':
     post_id = 22683750
-    loop = asyncio.get_event_loop()
-    results = loop.run_until_complete(main(loop, post_id))
+    results = asyncio.run(main(post_id))
     print(f'RESULTS - {results}')
