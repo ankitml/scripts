6962df690611b4b9eb431d20401026d8c000858c
Ankit <ankitml@gmail.com>
2020-03-25 13:10:57 -0400

    fetching number of comments in linear way

--- python/async/first.py
+++ python/async/first.py
@@ -4,12 +4,20 @@ import aiohttp
 URL_TEMPLATE = "https://hacker-news.firebaseio.com/v0/item/{}.json"
 
 
+async def fetch_numberof_comments(session, post_id):
+    url = URL_TEMPLATE.format(post_id)
+    async with session.get(url) as response:
+        response_text = await response.json()
+        return len(response_text['kids'])
+
+
 async def main(post_id):
-    await asyncio.sleep(1)
-    return 5
+    async with aiohttp.ClientSession() as session:
+        result = await fetch_numberof_comments(session, post_id)
+        return result
 
 
 if __name__ == '__main__':
     post_id = 22683750
     results = asyncio.run(main(post_id))
-    print(f'RESULTS - {results}')
+    print(f'Number of comments - {results}')
