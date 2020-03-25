109892c5ee253ceb24f99fa7068dbf85eab229cb
Ankit <ankitml@gmail.com>
2020-03-25 13:24:58 -0400

    recursive coroutines with gather

--- python/async/first.py
+++ python/async/first.py
@@ -8,7 +8,16 @@ async def fetch_numberof_comments(session, post_id):
     url = URL_TEMPLATE.format(post_id)
     async with session.get(url) as response:
         response_text = await response.json()
-        return len(response_text['kids'])
+        if 'kids' not in response_text:
+            return 0
+        kids = response_text['kids']
+        number_of_comments = len(kids)
+        tasks = [fetch_numberof_comments(session, kid_id)
+                 for kid_id in kids]
+        kid_results = await asyncio.gather(*tasks)
+        number_of_comments += sum(kid_results)
+        print(f'POST id {post_id} has {len(kids)} no of kids')
+        return number_of_comments
 
 
 async def main(post_id):
