4852871badee6116610173dfb67a4ce240442809
Ankit <ankitml@gmail.com>
2020-03-26 02:24:47 -0400

    5 subscription levels

--- python/async/redis/subscribe.py
+++ python/async/redis/subscribe.py
@@ -4,6 +4,8 @@ SUBSCRIBE TO REDIS PUBSUB as a generator
 
 import aioredis
 import asyncio
+from collections import deque
+from statistics import mean
 
 CHANNEL = "first"
 
@@ -30,15 +32,48 @@ async def subscribe2():
     while True:
         await channel.wait_message()
         msg = await channel.get(encoding='utf-8')
-        yield msg
+        yield int(msg)
 
-async def main():
+
+async def subscribe3():
+    """
+    print average of all seen numbers
+    """
+    n = 0
+    s = 0
     async for msg in subscribe2():
-        print(msg)
+        n += 1
+        s += msg
+        print(f"Average of {n} numbers is {s/n:.3f}")
+
+
+async def subscribe4(n=10):
+    """
+    moving average of last 10 numbers in the channel
+    """
+    data = deque(maxlen=n)
+    async for msg in subscribe2():
+        data.append(msg)
+        m = mean(data)
+        print(f"Average of last {len(data)} numbers is {m:.3f}")
+
+
+async def subscribe5():
+    """
+    squares, filters above 60 and maintains average of last 10, compare with only average of last 10
+    """
+    data1 = deque(maxlen=10)
+    data2 = deque(maxlen=10)
+    async for msg in subscribe2():
+        data2.append(msg)
+        msg_sq = msg*msg
+        if msg_sq >= 60:
+            data1.append(msg_sq)
+        print(f"Average of last {len(data1)} squares above 60 is {mean(data1)} and generic average is {mean(data2)}")
+    
 
 
 if __name__=='__main__':
-
-    asyncio.run(main())
+    asyncio.run(subscribe5())
 
 
